88a267f3e21973edf48a57f27f795d4fa59e3f4a
KainTheDev <thica.doi@gmail.com>
2023-09-09 21:44:42 +0700

    .

--- utils/randomName.js
+++ utils/randomName.js
@@ -7,10 +7,11 @@ const { readFileSync } = require("fs")
  * @returns
  */
 async function randomName (Your_names_list, type) {
-  if(typeof Your_names_list !== "object") console.trace("Your_names_list must be array."), console.exit(0);
-  let nameList = readFileSync("data/names.txt", "utf-8")
+  if(Your_names_list && typeof Your_names_list !== "object") console.trace("Your_names_list must be array."), console.exit(0);
+  let nameList = readFileSync("data/names.txt", "utf-8").split("\n")
   if(Your_names_list) nameList = Your_names_list
   var result = nameList[Math.floor(Math.random()*nameList.length)]
+  if(!type) type === 'string'
   if(type === "string") result = String(result)
   if(type === "array") result = [result]
   if(type === "JSON") result = {name: result}
