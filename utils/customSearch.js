81ff2198d148bf456028c94066285836ae2911ab
SoulYT1534 <thica.doi@gmail.com>
2023-11-30 14:46:17 +0700

    .

--- utils/customSearch.js
+++ utils/customSearch.js
@@ -2,7 +2,7 @@ const { default: fetch } = require('node-fetch');
 /**
  * 
  * @param {String} query query to search
- * @param {{safe_search: boolean, image_search: boolean, image_size: 'huge' | 'icon' | 'large' | 'medium' | 'small' | 'xlarge' | 'xxlarge'}} config configure other options. More information: https://developers.google.com/custom-search/v1/reference/rest
+ * @param {{safe_search: boolean}} config configure other options. More information: https://developers.google.com/custom-search/v1/reference/rest
  * @param {String} apiKey API key from googleapis
  * @param {String} searchEngineId search engine ID from googleapis
  * @returns {JSON}
@@ -12,10 +12,11 @@ async function customSearch(query, apiKey, searchEngineId, config = {}) {
   if(config['safe_search'] === true) safeSearchOption = '&safe=active'
   if(config['safe_search'] === false) safeSearchOption = '&safe=off'
   if(config['safe_search'] === undefined) safeSearchOption = ''
-  const apiUrl = `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=${searchEngineId}&q=${query}${config['image_search'] ? `&searchType=image` : ''}${safeSearchOption}${config['image_size'] ? `&imgSize=${config['image_size']}` : ''}`;
+  const apiUrl = `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=${searchEngineId}&q=${query}${safeSearchOption}`;
   return fetch(apiUrl)
     .then(async response => {
       const data = await response.json()
+      data.url = apiUrl
       return data;
     })
     .catch(error => {
