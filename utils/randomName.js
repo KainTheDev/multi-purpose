18213e35cc46fcd33afa601a2e91ecc6b90c3523
SoulYT1534 <thica.doi@gmail.com>
2023-12-12 12:42:10 +0700

    .

--- utils/randomName.js
+++ utils/randomName.js
@@ -2,22 +2,24 @@ const { default: fetch } = require("node-fetch");
 
 /**
  * @param {"firstname" | "surname" | "fullname"} [type]
- * @param {number} [amount] 
- * @param {Array} Your_names_list 
+ * @param {string} apiKey 
+ * @param {Array} Your_names_list
  * @returns 
  */
-async function randomName(type='fullname', amount=5000, Your_names_list) {
-  if (Your_names_list && typeof Your_names_list !== "object") throw console.trace("Your_names_list parameter must be array.");
-  if(amount > 5000) throw console.trace('Amount must be under or equal to 5000.');
+async function randomName(type = 'fullname', apiKey, Your_names_list) {
+  if (Your_names_list && typeof Your_names_list !== "object" && !Your_names_list.length) throw console.trace("Your_names_list parameter must be array.");
   const response = await fetch(`https://randommer.io/api/Name?nameType=${type}&quantity=${amount}`, {
     headers: {
-      'X-Api-Key': '21bd79236f314f828fd25ad415ae5c3f'
+      'X-Api-Key': apiKey
     }
   })
   const data = await response.json()
   if (data.status) throw console.trace(data);
-  const nameList = data
-  return nameList[Math.floor(Math.random() * nameList.length)]
+  const namesList = data
+  for (const name of Your_names_list) {
+    namesList.push(name)
+  }
+  return namesList[Math.floor(Math.random() * namesList.length)]
 }
 
 module.exports = randomName
